45f286d04206f48a5c0aa2ee9cab935940ae7ddd
聂秋荣 <nieqiurong@163.com>
2025-05-26 16:49:12 +0800

    OracleDdlGenerator支持指定schema模式运行.
    https://github.com/baomidou/mybatis-plus/issues/6656

--- mybatis-plus-extension/src/test/java/com/baomidou/mybatisplus/test/DdlHelperTest.java
+++ mybatis-plus-extension/src/test/java/com/baomidou/mybatisplus/test/DdlHelperTest.java
@@ -37,7 +37,7 @@ public class DdlHelperTest {
         var dataSource = new UnpooledDataSource(org.postgresql.Driver.class.getName(),
             "jdbc:postgresql://localhost:5432/postgres",
             "postgres", "123456");
-        IDdlGenerator ddlGenerator = new PostgreDdlGenerator();
+        IDdlGenerator ddlGenerator = PostgreDdlGenerator.newInstance();
         DdlHelper.runScript(ddlGenerator, dataSource, List.of("ddl/test.sql"),
             true, DdlScriptErrorHandler.ThrowsErrorHandler.INSTANCE);
         // 指定scheme运行 旧版本的模式是指定 public,为了兼容当使用默认的示例是无法根据指定的模式走的
@@ -90,7 +90,7 @@ public class DdlHelperTest {
 
     @Test
     void testForH2() throws SQLException {
-        var dataSource = new UnpooledDataSource(org.sqlite.JDBC.class.getName(),
+        var dataSource = new UnpooledDataSource(org.h2.Driver.class.getName(),
             "jdbc:h2:mem:test;DATABASE_TO_LOWER=TRUE",
             "sa", "");
         DdlHelper.runScript(null, dataSource, List.of("ddl/test.sql"),
@@ -99,7 +99,7 @@ public class DdlHelperTest {
 
     @Test
     void testForH2Mysql() throws SQLException {
-        var dataSource = new UnpooledDataSource(org.sqlite.JDBC.class.getName(),
+        var dataSource = new UnpooledDataSource(org.h2.Driver.class.getName(),
             "jdbc:h2:mem:test;MODE=MySQL",
             "sa", "");
         var ddlGenerator = new MysqlDdlGenerator();
@@ -109,12 +109,28 @@ public class DdlHelperTest {
 
     @Test
     void testForH2Postgresql() throws SQLException {
-        var dataSource = new UnpooledDataSource(org.sqlite.JDBC.class.getName(),
+        var dataSource = new UnpooledDataSource(org.h2.Driver.class.getName(),
             "jdbc:h2:mem:test;MODE=PostgreSQL;DATABASE_TO_LOWER=TRUE",
             "sa", "");
         var ddlGenerator = new PostgreDdlGenerator();
         DdlHelper.runScript(ddlGenerator, dataSource, List.of("ddl/test.sql"),
             true, DdlScriptErrorHandler.ThrowsErrorHandler.INSTANCE);
     }
+
+    @Test
+    @Disabled
+    void testForDm() throws SQLException {
+        var dataSource = new UnpooledDataSource(dm.jdbc.driver.DmDriver.class.getName(),
+            "jdbc:dm://127.0.0.1:5236/DMSERVER",
+            "SYSDBA", "Dm123456");
+        var ddlGenerator = new OracleDdlGenerator();
+        DdlHelper.runScript(ddlGenerator, dataSource, List.of("ddl/test.sql"),
+            true, DdlScriptErrorHandler.ThrowsErrorHandler.INSTANCE);
+        // 指定模式运行
+        ddlGenerator = new OracleDdlGenerator("TEST1");
+        DdlHelper.runScript(ddlGenerator, dataSource, List.of("ddl/test.sql"),
+            true, DdlScriptErrorHandler.ThrowsErrorHandler.INSTANCE);
+    }
+
 
 }
